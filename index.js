2680b4d4b3bf20d7104f317cbd220591aa3ec51c
Johanna Lehto <johannaeveliinalehto@gmail.com>
2023-01-24 21:16:47 +0200

    Part 3 Phonebook, task 3.10: Add fly.io launches etc for deploying the app

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan('tiny'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
@@ -112,4 +113,5 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
 
